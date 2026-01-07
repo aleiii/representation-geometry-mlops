dbe8876edba44b992160fe314aa83ea16681d3ec
aleiii <lightning6327@proton.me>
2026-01-07 23:10:15 +0100

    init cookiecutter project

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for representation_geometry
---
+++ src/representation_geometry/train.py
@@ -0,0 +1,10 @@
+from representation_geometry.model import Model
+from representation_geometry.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
